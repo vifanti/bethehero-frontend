c56a79847510a948914862ed08d003fd02d0fda9
Vinicius Fantinatto <vifanti@gmail.com>
2020-03-31 00:38:27 -0300

    Deploy front end

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./global.css";
+
+import Routes from "./routes";
+
+function App() {
+  /**
+   * Quando usamos o useState, ele retorna um Array com duas posições
+   *  [valor, funcaoDeAtualização]
+   */
+
+  return <Routes />;
+}
+
+export default App;
