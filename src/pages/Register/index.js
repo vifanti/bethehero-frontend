9cd010df40509a7a82d4b8b3b552ecb0a5f97ddc
Vinicius Fantinatto <vifanti@gmail.com>
2020-09-23 11:23:09 -0300

    correçao frase tela cadastro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -45,7 +45,7 @@ export default function Register() {
           <img src={logoImg} alt="Be The Hero" />
           <h1>Cadastro</h1>
           <p>
-            Faça seu cadastro, entre na plataforma e ajude pessoas a entrarem os
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os
             casos da sua ONG.
           </p>
           <Link className="back-link" to="/">
